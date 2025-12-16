1a02183f94105c79a1ddec4e8abef676b79cf168
Malcolm Wright <m.j.wright@massey.ac.nz>
2025-12-17 12:56:38 +1300

    added static files

--- restaurant/views.py
+++ restaurant/views.py
@@ -5,3 +5,5 @@ def sayHello(request):
     return HttpResponse("Hello from the Little Lemon Restaurant!")
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html', {})
