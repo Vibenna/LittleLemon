2a506977a14f32b2f868e18f96d071c5294ed850
Malcolm Wright <m.j.wright@massey.ac.nz>
2025-12-17 12:01:14 +1300

    initial migrations

---
+++ restaurant/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def sayHello(request):
+    return HttpResponse("Hello from the Little Lemon Restaurant!")
+
+# Create your views here.
