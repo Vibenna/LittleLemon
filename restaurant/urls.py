cf8ffb9978feb690d5c45dad235736ea937bea68
Malcolm Wright <m.j.wright@massey.ac.nz>
2025-12-17 17:54:20 +1300

    set up table booking api

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,12 +1,13 @@
-from django.contrib import admin 
-from django.urls import path 
-from . import views
 
+from django.urls import path, include
+from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'booking', views.bookingViewSet, basename='booking')
 
 urlpatterns = [ 
-    # path('', views.sayHello, name='sayHello'), 
-    # path('', views.index, name='index'),
     path('menu/', views.menuItemsView.as_view(), name='menu'),
     path('menu/<int:pk>/', views.singleMenuItemView.as_view(), name='single-menu-item'),
-    # path('booking/', views.bookingView.as_view(), name='bookings'),
+    path('', include(router.urls)),
 ]
